dc2d8ee661c9a47ff5addb4cd02fbfe9bb5fb52a
huntj0n <hunt.jon19@gmail.com>
2021-07-18 10:12:36 -0600

    Home sections

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,54 @@ import Section from './Section';
 function Home() {
     return (
         <Container>
-            <Section />
-            <Section />
-            <Section />
+            <Section 
+                title="Model S"
+                desc="Order Online for Touchless Delivery"
+                backgroundImg="model-s.jpg"
+                leftBtnText="custom order"
+                rightBtnText="existing inventory"
+            />
+            <Section 
+                title="Model Y"
+                desc="Order Online for Touchless Delivery"
+                backgroundImg="model-y.jpg"
+                leftBtnText="custom order"
+                rightBtnText="existing inventory"
+            />
+            <Section 
+                title="Model 3"
+                desc="Order Online for Touchless Delivery"
+                backgroundImg="model-3.jpg"
+                leftBtnText="custom order"
+                rightBtnText="existing inventory"
+            />
+            <Section 
+                title="Model X"
+                desc="Order Online for Touchless Delivery"
+                backgroundImg="model-x.jpg"
+                leftBtnText="custom order"
+                rightBtnText="existing inventory"
+            />
+            <Section 
+                title="Solar Panels"
+                desc="Lowest Cost Solar Panels in America"
+                backgroundImg="solar-panel.jpg"
+                leftBtnText="order now"
+                rightBtnText="Learn More"
+            />
+            <Section 
+                title="Solar Roof"
+                desc="Produce Clean Energy From Your Roof"
+                backgroundImg="solar-roof.jpg"
+                leftBtnText="order now"
+                rightBtnText="Learn More"
+            />
+            <Section 
+                title="Accessories"
+                desc="Order Online for Touchless Delivery"
+                backgroundImg="accessories.jpg"
+                leftBtnText="Shop Now"
+            />
         </Container>
     )
 }
